8eede692ed6260b16be54f2c7216048f2c5352fd
kpilszak <klaudia.pilszak@gmail.com>
2022-11-20 18:36:45 +0100

    Init repo

---
+++ src/main/java/com/kpilszak/booksandstuffecommerce/BooksAndStuffEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.booksandstuffecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAndStuffEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAndStuffEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpilszak/booksandstuffecommerce/BooksAndStuffEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.booksandstuffecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAndStuffEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
