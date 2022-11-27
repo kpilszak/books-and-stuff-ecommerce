bcbd75b9ce8e31342df214b72ad05ace90684886
kpilszak <klaudia.pilszak@gmail.com>
2022-11-27 17:18:34 +0100

    Map Product entity to database fields

--- src/main/java/com/kpilszak/booksandstuffecommerce/entity/Product.java
+++ src/main/java/com/kpilszak/booksandstuffecommerce/entity/Product.java
@@ -1,10 +1,10 @@
 package com.kpilszak.booksandstuffecommerce.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -14,15 +14,28 @@ import java.util.Date;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "sku")
     private String sku;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
+    @Column(name = "image_url")
     private String imageUrl;
+    @Column(name = "active")
     private boolean active;
+    @Column(name = "units_in_stock")
     private int unitsInStock;
+    @Column(name = "date_created")
+    @CreationTimestamp
     private Date dateCreated;
+    @Column(name = "last_updated")
+    @UpdateTimestamp
     private Date lastUpdated;
 
 }
