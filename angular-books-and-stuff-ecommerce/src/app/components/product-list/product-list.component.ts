7037b93a8d1ec3e51252ef69ee1ae4590a04e847
kpilszak <klaudia.pilszak@gmail.com>
2023-10-01 12:20:56 -0400

    Call cart service from product list component

--- angular-books-and-stuff-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-books-and-stuff-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../../services/product.service";
 import { Product } from "../../common/product";
 import { ActivatedRoute } from "@angular/router";
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -21,7 +23,7 @@ export class ProductListComponent implements OnInit {
 
   previousKeyword: string = "";
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private cartService: CartService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -91,6 +93,10 @@ export class ProductListComponent implements OnInit {
 
   addToCart(product: Product) {
     console.log(`Addingto cart: ${product.name}, ${product.unitPrice}`);
+
+    const cartItem = new CartItem(product);
+
+    this.cartService.addToCart(cartItem);
   }
 
 }
