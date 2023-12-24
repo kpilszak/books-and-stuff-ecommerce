1313b341dc56a8de2a4fa00d16907a00a59ae326
kpilszak <klaudia.pilszak@gmail.com>
2023-12-24 05:55:21 -0500

    Checkout form: billing address. Copy when the same

--- angular-books-and-stuff-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-books-and-stuff-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -44,6 +44,15 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  copyShippingAddressToBillingAddress(event: any) {
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+      .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')!.value);
