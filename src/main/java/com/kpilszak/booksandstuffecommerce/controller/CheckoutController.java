7a386b03a6848a5252d88cfb0d137e01ecadfd26
kpilszak <klaudia.pilszak@gmail.com>
2024-12-30 10:02:00 -0500

    Create purchase controller

---
+++ src/main/java/com/kpilszak/booksandstuffecommerce/controller/CheckoutController.java
@@ -0,0 +1,24 @@
+package com.kpilszak.booksandstuffecommerce.controller;
+
+import com.kpilszak.booksandstuffecommerce.dto.Purchase;
+import com.kpilszak.booksandstuffecommerce.dto.PurchaseResponse;
+import com.kpilszak.booksandstuffecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private final CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
