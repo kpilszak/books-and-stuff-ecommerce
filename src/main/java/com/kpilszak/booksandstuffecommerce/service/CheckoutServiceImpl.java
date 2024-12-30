a7ee52f91a7fc416e71c758bbe6f5a0702910de5
kpilszak <klaudia.pilszak@gmail.com>
2024-12-30 09:55:49 -0500

    Implement place order logic

--- src/main/java/com/kpilszak/booksandstuffecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/kpilszak/booksandstuffecommerce/service/CheckoutServiceImpl.java
@@ -3,7 +3,14 @@ package com.kpilszak.booksandstuffecommerce.service;
 import com.kpilszak.booksandstuffecommerce.dao.CustomerRepository;
 import com.kpilszak.booksandstuffecommerce.dto.Purchase;
 import com.kpilszak.booksandstuffecommerce.dto.PurchaseResponse;
+import com.kpilszak.booksandstuffecommerce.entity.Customer;
+import com.kpilszak.booksandstuffecommerce.entity.Order;
+import com.kpilszak.booksandstuffecommerce.entity.OrderItem;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -12,8 +19,30 @@ public class CheckoutServiceImpl implements CheckoutService {
     public CheckoutServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
+
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+        Order order = purchase.getOrder();
+
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        order.setShippingAddress(purchase.getShippingAddress());
+        order.setBillingAddress(purchase.getBillingAddress());
+
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        customerRepository.save(customer);
+
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+        return UUID.randomUUID().toString();
     }
 }
