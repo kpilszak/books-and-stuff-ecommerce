c7c85e10866154ea257e2f3c5f6cd360a5bea0db
kpilszak <klaudia.pilszak@gmail.com>
2025-01-13 09:24:32 -0500

    Add Okta integration

---
+++ angular-books-and-stuff-ecommerce/src/app/config/app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: 'D9Obffv0NqcFMMvGCUigSUutwwzdpe5g',
+    issuer: 'https://dev-2efd6xy2mb5fbs8k.us.auth0.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
