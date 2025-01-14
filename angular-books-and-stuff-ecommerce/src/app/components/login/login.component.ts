6b137cdfad7d7cd7a27435adbc9110b7d8bc5e68
kpilszak <klaudia.pilszak@gmail.com>
2025-01-14 09:38:37 -0500

    Create Login component

---
+++ angular-books-and-stuff-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import appConfig from '../../config/app-config';
+import { OKTA_AUTH } from "@okta/okta-angular";
+import { OktaAuth } from "@okta/okta-auth-js";
+import OktaSignIn from "@okta/okta-signin-widget";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignin: any;
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: appConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: appConfig.oidc.clientId,
+      redirectUri: appConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: appConfig.oidc.issuer,
+        scopes: appConfig.oidc.scopes
+      }
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
