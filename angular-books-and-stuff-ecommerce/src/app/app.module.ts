7030cd33cdecf5beb0bc60b6e9fbbb90bbfc66c2
kpilszak <klaudia.pilszak@gmail.com>
2025-01-20 15:19:53 -0500

    Add login routes

--- angular-books-and-stuff-ecommerce/src/app/app.module.ts
+++ angular-books-and-stuff-ecommerce/src/app/app.module.ts
@@ -18,8 +18,17 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import appConfig from './config/app-config';
+
+const oktaConfig = appConfig.oidc;
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
+  { path: 'login/callback', component: OktaCallbackComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'cart-details', component: CartDetailsComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
