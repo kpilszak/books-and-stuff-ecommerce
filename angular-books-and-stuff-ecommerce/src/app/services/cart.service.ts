e876d10f6ef2e6f41e1e7aa4544bbf9080de4af9
kpilszak <klaudia.pilszak@gmail.com>
2025-01-28 07:29:38 -0500

    Persist cart data in session storage

--- angular-books-and-stuff-ecommerce/src/app/services/cart.service.ts
+++ angular-books-and-stuff-ecommerce/src/app/services/cart.service.ts
@@ -54,6 +54,8 @@ export class CartService {
     this.totalQuantity.next(totalQuantityValue);
 
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    this.persistCartItems();
   }
 
   persistCartItems() {
