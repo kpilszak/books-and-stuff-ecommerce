58da777be3d33bb612d2b54b802b18111f1c1938
kpilszak <klaudia.pilszak@gmail.com>
2025-02-03 14:22:21 -0500

    refactor: Configure cors mapping

--- src/main/java/com/kpilszak/booksandstuffecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/kpilszak/booksandstuffecommerce/config/MyDataRestConfig.java
@@ -5,6 +5,7 @@ import com.kpilszak.booksandstuffecommerce.entity.Product;
 import com.kpilszak.booksandstuffecommerce.entity.ProductCategory;
 import com.kpilszak.booksandstuffecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,6 +20,9 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
 
     private EntityManager entityManager;
 
@@ -38,7 +42,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         exposeIds(config);
 
-        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(allowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config,
