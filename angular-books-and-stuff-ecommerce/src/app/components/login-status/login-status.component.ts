700e8e5bf39d7f105f97d95aa251049c4af77563
kpilszak <klaudia.pilszak@gmail.com>
2025-02-10 15:26:39 -0500

    Add order history service

--- angular-books-and-stuff-ecommerce/src/app/components/login-status/login-status.component.ts
+++ angular-books-and-stuff-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -12,6 +12,8 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string = '';
 
+  storage: Storage = sessionStorage;
+
   constructor(private oktaAuthService: OktaAuthStateService,
               @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
@@ -29,6 +31,9 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuth.getUser().then(
         (res) => {
           this.userFullName = res.name as string;
+
+          const email = res.email;
+          this.storage.setItem('userEmail', JSON.stringify(email));
         }
       );
     }
