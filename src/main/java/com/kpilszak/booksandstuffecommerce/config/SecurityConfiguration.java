f935b62a9d78d26be9776ab667639688cab4ef00
kpilszak <klaudia.pilszak@gmail.com>
2025-02-18 16:53:33 -0500

    Add security filter chain

---
+++ src/main/java/com/kpilszak/booksandstuffecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.kpilszak.booksandstuffecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(configurer -> configurer
+                .antMatchers("/api/orders/**")
+                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
+
+        http.cors();
+
+        return http.build();
+    }
+}
