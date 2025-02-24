c3169083071309a6b6a8ed429ff230cc1b247ecd
kpilszak <klaudia.pilszak@gmail.com>
2025-02-24 05:49:05 -0500

    Secure endpoints on frontend

--- angular-books-and-stuff-ecommerce/src/app/services/auth-interceptor.service.ts
+++ angular-books-and-stuff-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,19 +1,32 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { from, Observable } from "rxjs";
+import { Inject, Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { from, lastValueFrom, Observable } from 'rxjs';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return from(this.handleAccess(request, next));
   }
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-    throw new Error('Method not implemented.')
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      const accessToken = this.oktaAuth.getAccessToken();
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+
+    return await lastValueFrom(next.handle(request));
   }
 }
