f20b554d7f8d5fe61dc2be7817ac8cb5fd116bf2
kpilszak <klaudia.pilszak@gmail.com>
2025-02-26 07:58:12 -0500

    Update protocol in redirectUri to https

--- angular-books-and-stuff-ecommerce/src/app/config/app-config.ts
+++ angular-books-and-stuff-ecommerce/src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: 'D9Obffv0NqcFMMvGCUigSUutwwzdpe5g',
     issuer: 'https://dev-2efd6xy2mb5fbs8k.us.auth0.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
