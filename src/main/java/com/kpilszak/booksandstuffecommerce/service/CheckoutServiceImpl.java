ee0fc699eb0cd6130781c6e8362dd5c29c5c6e75
kpilszak <klaudia.pilszak@gmail.com>
2025-04-01 16:21:07 -0400

    fix: Purchase description

--- src/main/java/com/kpilszak/booksandstuffecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/kpilszak/booksandstuffecommerce/service/CheckoutServiceImpl.java
@@ -65,6 +65,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "Your purchase");
 
         return PaymentIntent.create(params);
     }
